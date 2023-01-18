b0499b7440e087e6831e816eb9d802bf8659923d
Daniel-K-Ivanov <daniel.k.ivanov95@gmail.com>
2023-01-18 21:28:23 +0200

    add test run sh script
    Signed-off-by: Daniel-K-Ivanov <daniel.k.ivanov95@gmail.com>

---
+++ circuits/verify_ssz_to_poseidon_commitment/run.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo "****COMPILING CIRCUIT****"
+start=`date +%s`
+circom test_sync_committee_committments.circom --O1 --r1cs --sym --c
+end=`date +%s`
+echo "DONE ($((end-start))s)"
+
