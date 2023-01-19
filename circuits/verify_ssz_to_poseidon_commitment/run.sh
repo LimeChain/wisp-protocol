3d0219758470ffeb778060f76e7ebb8cef67345a
Daniel-K-Ivanov <daniel.k.ivanov95@gmail.com>
2023-01-19 09:11:40 +0200

    modify sh script
    Signed-off-by: Daniel-K-Ivanov <daniel.k.ivanov95@gmail.com>

--- circuits/verify_ssz_to_poseidon_commitment/run.sh
+++ circuits/verify_ssz_to_poseidon_commitment/run.sh
@@ -2,7 +2,7 @@
 
 echo "****COMPILING CIRCUIT****"
 start=`date +%s`
-circom test_sync_committee_committments.circom --O1 --r1cs --sym --c
+circom test_sync_committee_committments.circom --O1 --r1cs --sym --c --output result
 end=`date +%s`
 echo "DONE ($((end-start))s)"
 
