d9d6671e90d995ca340f941c23ae451965f42736
George Spasov <george@limechain.tech>
2023-02-20 13:40:58 +0200

    Added CRCOutbox contract, the interface for sending a CRCMessage and the Type for CRCMessage

---
+++ contracts/src/crc-messages/CRCOutbox.sol
@@ -0,0 +1,65 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+import {ICRCOutbox} from "./interfaces/ICRCOutbox.sol";
+import {Types} from "./libraries/Types.sol";
+
+contract CRCOutbox is ICRCOutbox {
+    /// @dev outbox for messages
+    bytes32[] public outbox;
+
+    /// @dev getting index by the message hash
+    mapping(bytes32 => uint256) public indexOf;
+
+    /// @dev used to nullify nonces
+    mapping(address => mapping(uint64 => bool)) public noncesNullifier;
+
+    event MessageSent(
+        address indexed sender,
+        bytes32 indexed hash,
+        uint256 messageIndex
+    );
+
+    /// @notice sends CRC message. Stores it as keccak hash inside the outbox
+    /// @dev Complete properties of a CRC message used for the hash are
+    ///     uint8 - version
+    ///     uint256 - chainId
+    ///     uint256 - destination chainId
+    ///     address - message sender
+    ///     address - user - actual sender
+    function sendMessage(Types.CRCMessage calldata message)
+        public
+        returns (bytes32 messageHash)
+    {
+        require(
+            !noncesNullifier[msg.sender][message.nonce],
+            "Nonce already used"
+        );
+
+        uint256 messageIndex = outbox.length;
+
+        noncesNullifier[msg.sender][message.nonce] = true;
+
+        messageHash = keccak256(
+            abi.encode(
+                message.version,
+                message.destinationChainId,
+                message.nonce,
+                msg.sender,
+                message.user,
+                message.target,
+                message.payload,
+                message.stateRelayFee,
+                message.deliveryFee,
+                message.extra
+            )
+        );
+
+        outbox.push(messageHash);
+        indexOf[messageHash] = messageIndex;
+
+        emit MessageSent(msg.sender, messageHash, messageIndex);
+
+        return messageHash;
+    }
+}
