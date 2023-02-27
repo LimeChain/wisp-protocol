0fc33c07da865ebaab2f00df6f253d78a5ee3d67
George Spasov <george@limechain.tech>
2023-02-27 09:19:37 +0200

    Added onlyOwner for setting CRCOutboxes and chainIds

--- contracts/src/crc-messages/CRCInbox.sol
+++ contracts/src/crc-messages/CRCInbox.sol
@@ -2,8 +2,9 @@
 pragma solidity ^0.8.13;
 
 import {Types as CRCTypes} from "./libraries/Types.sol";
+import {Owned} from "solmate/auth/Owned.sol";
 
-abstract contract CRCInbox {
+abstract contract CRCInbox is Owned {
     event InvokeSuccess(address indexed target, bytes32 indexed hash);
     event InvokeFailure(address indexed target, bytes32 indexed hash);
 
@@ -19,9 +20,12 @@ abstract contract CRCInbox {
     /// @notice Maps address of CRCOutbox in a certain source rollup or chain to its chainId
     mapping(address => uint256) public sourceChainIdFor;
 
+    constructor() Owned(msg.sender) {}
+
     /// @notice Sets the chainid for CRCOutbox
-    function setChainIdFor(address outbox, uint256 chainId) public {
-        // TODO add security
+    /// @param outbox the CRCOutbox contract address at the rollup
+    /// @param chainId the chainId of the rollup
+    function setChainIdFor(address outbox, uint256 chainId) public onlyOwner {
         // TODO in the future this should be improved as ideally outbox contracts have the same address on multiple networks
         sourceChainIdFor[outbox] = chainId;
     }
